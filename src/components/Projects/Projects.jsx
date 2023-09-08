66282d28d2e9beac0647c167ae6fa399914ff776
Federico <federicoc03@gmail.com>
2023-09-08 15:43:01 -0500

    feat:

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,17 +1,30 @@
 import MainProject from "./MainProject";
-import "./Projects.scss"
+import "./Projects.scss";
 
 export default function Projects() {
-
   const ricaCab = {
-    name : "RICA CAB APP",
-    description : "Rica is a transportation app developed combining React and Express. It connects passengers with registered drivers and fleet owners, offering a straightforward way to request rides and manage transportation schedules. Passengers can easily find and schedule trips, while drivers and fleet owners can efficiently accept requests.",
-  }
+    name: "RICA CAB APP",
+    description:
+      "Transportation app developed combining React and Express. It connects passengers with registered drivers and fleet owners, offering a straightforward way to request rides and manage transportation schedules. Passengers can easily find and schedule trips, while drivers and fleet owners can efficiently accept requests.",
+  };
 
-
+  const shakeMeet = {
+    name: "Shakemeet",
+    description:
+      "ShakeMeet is a web app designed for peer-to-peer connections among users within the same network. Developed using React, Tailwind CSS, and Firebase as the database, it incorporates a STUN server for efficient communication. In the future, the plan is to establish a dedicated STUN server to ensure seamless performance. ",
+  };
   return (
     <main className="projects">
-      <MainProject source={"/test.JPG"} name={ricaCab.name} description={ricaCab.description} />
+      <MainProject
+        source={"/RICA.JPG"}
+        name={ricaCab.name}
+        description={ricaCab.description}
+      />
+      <MainProject
+        source={"/SM.JPG"}
+        name={shakeMeet.name}
+        description={shakeMeet.description}
+      />
     </main>
   );
 }
