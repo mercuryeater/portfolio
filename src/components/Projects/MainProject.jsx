cd264b605a7ad8b711e31a3afbde8e5935dccd80
Federico <federicoc03@gmail.com>
2023-09-08 15:44:18 -0500

    feat: :lipstick: projectsResponsiveDone

--- src/components/Projects/MainProject.jsx
+++ src/components/Projects/MainProject.jsx
@@ -1,23 +1,30 @@
 import Image from "next/image";
-import "./MainProject.scss"
+import Link from "next/link";
+import "./MainProject.scss";
 
-export default function MainProject({source, name, description}) {
-
-  const imageStyle ={
-    width: "100%",
-    objectFit : 'cover',
-    position : 'relative',
-    height : 'auto',
-  }
+export default function MainProject({ source, name, description, url }) {
+  const imageStyle = {
+    maxWidth: "100%",
+    objectFit: "cover",
+    maxHeight: "100%",
+  };
   return (
-    <div className="project">
-      <div className="project__thumb" >
-        <Image src={source} alt="Test" width={959} height={516} style={imageStyle}/>
+    <Link className="link" href={url} target="_blank">
+      <div className="project">
+        <div className="project__thumb">
+          <Image
+            src={source}
+            alt="Test"
+            width={959}
+            height={516}
+            style={imageStyle}
+          />
+        </div>
+        <div className="project__texts">
+          <h2 className="project__texts__name">{name}</h2>
+          <p className="project__texts__description">{description}</p>
+        </div>
       </div>
-      <div className="project__texts">
-        <h2 className="project__name">{name}</h2>
-        <p>{description}</p>
-      </div>
-    </div>
+    </Link>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,12 +6,14 @@ export default function Projects() {
     name: "RICA CAB APP",
     description:
       "Transportation app developed combining React and Express. It connects passengers with registered drivers and fleet owners, offering a straightforward way to request rides and manage transportation schedules. Passengers can easily find and schedule trips, while drivers and fleet owners can efficiently accept requests.",
+    url: "https://cab-frontend.vercel.app/",
   };
 
   const shakeMeet = {
-    name: "Shakemeet",
+    name: "SHAKEMEET",
     description:
       "ShakeMeet is a web app designed for peer-to-peer connections among users within the same network. Developed using React, Tailwind CSS, and Firebase as the database, it incorporates a STUN server for efficient communication. In the future, the plan is to establish a dedicated STUN server to ensure seamless performance. ",
+    url: "https://shakemeet-develop.onrender.com/",
   };
   return (
     <main className="projects">
@@ -19,11 +21,13 @@ export default function Projects() {
         source={"/RICA.JPG"}
         name={ricaCab.name}
         description={ricaCab.description}
+        url={ricaCab.url}
       />
       <MainProject
         source={"/SM.JPG"}
         name={shakeMeet.name}
         description={shakeMeet.description}
+        url={shakeMeet.url}
       />
     </main>
   );
