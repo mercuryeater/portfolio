5fe6f61edc768f29027a7fcc779358841ec12e27
Federico Córdoba <79395978+mercuryeater@users.noreply.github.com>
2023-09-09 13:52:11 -0500

    feat: :sparkles: addIntroNMockContact (#5)

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,43 @@
+"use client";
+import "./Contact.scss";
+
 export default function Contact() {
-    return (
-      <div>
-        <h2>Contact me</h2>
-        <form > 
-            <input type="text" />
-        </form>
-      </div>
-    );
-  }
-  
+  const sendForm = (e) => {
+    e.preventDefault();
+    console.log("sending form");
+
+    const formData = new FormData(e.target);
+    const message = Object.fromEntries(formData);
+    console.log(message);
+  };
+
+  return (
+    <div className="contact">
+      <h2 className="contact__title">Coctact me</h2>
+      <p className="contact__title">I would love to work with you 👀</p>
+      <form className="" onSubmit={sendForm}>
+        <input
+          className="contact__input"
+          type="text"
+          name="name"
+          placeholder="Name"
+        />
+        <input
+          className="contact__input"
+          type="email"
+          placeholder="Email address"
+          name="email"
+        />
+        <input
+          className="contact__inputXl"
+          type="text"
+          placeholder="Tell me, how can I help you?"
+          name="message"
+        />
+        <button type="submit" className="contact__btn">
+          Send
+        </button>
+      </form>
+    </div>
+  );
+}
---
+++ src/components/Intro/Intro.jsx
@@ -0,0 +1,36 @@
+import { IconBrandGithubFilled, IconBrandLinkedin } from "@tabler/icons-react";
+import "./Intro.scss";
+
+export default function Intro() {
+  return (
+    <main className="intro">
+      <section className="intro__nameInfo">
+        <h1>Federico Cordoba</h1>
+        <h3>Full Stack Developer / Visual Artist</h3>
+        <div className="intro__nameInfo__buttons">
+          <a href="https://github.com/mercuryeater" target="_blank">
+            <IconBrandGithubFilled />
+            Github
+          </a>
+          <a
+            href="https://www.linkedin.com/in/federico-alejandro-cordoba-romero-7b898816a/"
+            target="_blank"
+          >
+            <IconBrandLinkedin />
+            LinkedIn
+          </a>
+        </div>
+      </section>
+
+      <p className="intro__description">
+        Proficient in developing web applications using
+        <strong> JavaScript/TypeScript</strong> in <strong> React</strong> and{" "}
+        <strong> Node.js</strong>, utilizing <strong> SQL</strong> (Postgres)
+        with Prisma, and <strong> NoSQL</strong> (MongoDB) with Mongoose.
+        Experience includes working in small teams following{" "}
+        <strong>SCRUM</strong> methodology and implementing{" "}
+        <strong> Git Flow</strong> for enhanced action flow and communication.
+      </p>
+    </main>
+  );
+}
