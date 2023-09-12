a292c131262978f1f4b18b56d8dabcfe4181c831
Federico Córdoba <79395978+mercuryeater@users.noreply.github.com>
2023-09-12 03:20:23 -0500

    Feature/general styles (#10)
    * chore: :lipstick: addColorsUnifyButtons
    
    * feat: :lipstick: addCustomScrollbar

--- src/components/Projects/MainProject.jsx
+++ src/components/Projects/MainProject.jsx
@@ -1,30 +1,43 @@
 import Image from "next/image";
 import Link from "next/link";
+import { IconBrandGithubFilled, IconWorldWww } from "@tabler/icons-react";
 import "./MainProject.scss";
 
-export default function MainProject({ source, name, description, url }) {
+export default function MainProject({
+  source,
+  name,
+  description,
+  webUrl,
+  repoUrl,
+}) {
   const imageStyle = {
     maxWidth: "100%",
     objectFit: "cover",
     maxHeight: "100%",
   };
   return (
-    <Link className="link" href={url} target="_blank">
-      <div className="project">
-        <div className="project__thumb">
-          <Image
-            src={source}
-            alt="Test"
-            width={959}
-            height={516}
-            style={imageStyle}
-          />
-        </div>
-        <div className="project__texts">
-          <h2 className="project__texts__name">{name}</h2>
-          <p className="project__texts__description">{description}</p>
-        </div>
+    <div className="project">
+      <div className="project__thumb">
+        <Image
+          src={source}
+          alt="Test"
+          width={959}
+          height={516}
+          style={imageStyle}
+        />
       </div>
-    </Link>
+      <div className="project__texts">
+        <span className="project__texts__name">
+          <h2>{name}</h2>
+          <Link href={repoUrl} target="_blank">
+            <IconBrandGithubFilled size={25} stroke={1} />
+          </Link>
+          <Link href={webUrl} target="_blank">
+            <IconWorldWww size={25} stroke={1} />
+          </Link>
+        </span>
+        <p className="project__texts__description">{description}</p>
+      </div>
+    </div>
   );
 }
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -6,14 +6,17 @@ export default function Projects() {
     name: "RICA CAB APP",
     description:
       "Transportation app developed combining React and Express. It connects passengers with registered drivers and fleet owners, offering a straightforward way to request rides and manage transportation schedules. Passengers can easily find and schedule trips, while drivers and fleet owners can efficiently accept requests.",
-    url: "https://cab-frontend.vercel.app/",
+    webUrl: "https://cab-frontend.vercel.app/",
+    repoUrl: "https://github.com/davidenco88/frontend-cab",
+    // https://github.com/davidenco88/backend-cab
   };
 
   const shakeMeet = {
     name: "SHAKEMEET",
     description:
       "ShakeMeet is a web app designed for peer-to-peer connections among users within the same network. Developed using React, Tailwind CSS, and Firebase as the database, it incorporates a STUN server for efficient communication. In the future, the plan is to establish a dedicated STUN server to ensure seamless performance. ",
-    url: "https://shakemeet-develop.onrender.com/",
+    webUrl: "https://shakemeet-develop.onrender.com/",
+    repoUrl: "https://github.com/mercuryeater/ShakeMeet",
   };
   return (
     <main className="projects">
@@ -21,13 +24,15 @@ export default function Projects() {
         source={"/RICA.JPG"}
         name={ricaCab.name}
         description={ricaCab.description}
-        url={ricaCab.url}
+        webUrl={ricaCab.webUrl}
+        repoUrl={ricaCab.repoUrl}
       />
       <MainProject
         source={"/SM.JPG"}
         name={shakeMeet.name}
         description={shakeMeet.description}
-        url={shakeMeet.url}
+        webUrl={shakeMeet.webUrl}
+        repoUrl={shakeMeet.repoUrl}
       />
     </main>
   );
