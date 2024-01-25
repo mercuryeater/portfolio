23ca208025253521a1de9deb072b744172a37b4b
Federico Córdoba <79395978+mercuryeater@users.noreply.github.com>
2024-01-24 22:18:47 -0500

    Develop - Add RastroPeludo (#15)
    * fix: :adhesive_bandage: urlShakemeet-UpdateReadMe (#13)
    
    * Feature add rastro peludo (#14)
    
    * feat: :construction: addRastroPeludo
    
    * refactor: :lipstick: changeOrderAddLinktoProdRastroP

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -5,7 +5,7 @@ export default function Projects() {
   const ricaCab = {
     name: "RICA CAB APP",
     description:
-      "Transportation app developed combining React and Express. It connects passengers with registered drivers and fleet owners, offering a straightforward way to request rides and manage transportation schedules. Passengers can easily find and schedule trips, while drivers and fleet owners can efficiently accept requests.",
+      "Transportation app developed combining React sand Express. It connects passengers with registered drivers and fleet owners, offering a straightforward way to request rides and manage transportation schedules. Passengers can easily find and schedule trips, while drivers and fleet owners can efficiently accept requests.",
     webUrl: "https://cab-frontend.vercel.app/",
     repoUrl: "https://github.com/davidenco88/frontend-cab",
     // https://github.com/davidenco88/backend-cab
@@ -15,11 +15,26 @@ export default function Projects() {
     name: "SHAKEMEET",
     description:
       "ShakeMeet is a web app designed for peer-to-peer connections among users within the same network. Developed using React, Tailwind CSS, and Firebase as the database, it incorporates a STUN server for efficient communication. In the future, the plan is to establish a dedicated STUN server to ensure seamless performance. ",
-    webUrl: "https://shakemeet-develop.onrender.com/",
+    webUrl: "https://shakemeet.onrender.com/",
     repoUrl: "https://github.com/mercuryeater/ShakeMeet",
+  };
+
+  const rastroPeludo = {
+    name: "RASTRO PELUDO (WIP)",
+    description:
+      "Rastro Peludo is a web app designed to report lost or seen pets. It relies on the users reporting encounters with lost dogs or cats in the sreet to help the owners find them. It's designed mobile first and the UX and code is all mine. It's being developed using Figma, Next.js 13^, React and Sass.",
+    webUrl: "https://rastro-peludo.vercel.app/",
+    repoUrl: "https://github.com/mercuryeater/rastro-peludo",
   };
   return (
     <main className="projects">
+      <MainProject
+        source={"/RP.JPG"}
+        name={rastroPeludo.name}
+        description={rastroPeludo.description}
+        webUrl={rastroPeludo.webUrl}
+        repoUrl={rastroPeludo.repoUrl}
+      />
       <MainProject
         source={"/RICA.JPG"}
         name={ricaCab.name}
